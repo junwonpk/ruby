4a7de2b9b463b2b262bf782babd529b4bbecbcc4
Yunhe (John) Wang <yhjw88@gmail.com>
2018-02-25 14:20:45 -0800

    Added baselines and LSTM

---
+++ baseline/rubyRTBaseline.py
@@ -0,0 +1,71 @@
+# Ruby RT Baseline
+# How well does response time predict further responses?
+import json
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+import sys
+
+def softmaxRegression(trainComments, trainLabels, devComments, devLabels):
+    trainX = np.array(trainComments).reshape(-1, 1)
+    devX = np.array(devComments).reshape(-1, 1)
+
+    print "Starting Regression"
+    lr = LogisticRegression(
+            C=1.0,
+            # class_weight="balanced",
+            solver="sag",
+            max_iter=1000,
+            # multi_class="multinomial",
+            verbose=True)
+    lr.fit(trainX, trainLabels)
+    trainPreds = lr.predict(trainX)
+    print "Train Accuracy: {}".format(accuracy_score(trainLabels, trainPreds))
+    devPreds = lr.predict(devX)
+    print "Dev Accuracy: {}".format(accuracy_score(devLabels, devPreds))
+    print "Matrix: \n{}".format(confusion_matrix(devLabels, devPreds))
+
+
+def loadComments(filename, numComments, capBuckets=False):
+    # Create bucket lookup.
+    # bucketLookup = [0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3]
+    bucketLookup = [0]
+    while len(bucketLookup) < 1780:
+        bucketLookup.append(1)
+
+    # Bucket cap.
+    bucketCounts = [0 for i in range(5)]
+    if capBuckets:
+        bucketCap = numComments / 5
+    else:
+        bucketCap = float('inf')
+
+    # Load comments.
+    comments = []
+    labels = []
+    with open(filename, "r") as inFile:
+        for line in inFile:
+            if len(comments) >= numComments:
+                break
+            comment = json.loads(line)
+            bucket = bucketLookup[comment["num_child_comments"]]
+            if bucketCounts[bucket] >= bucketCap:
+                continue
+            bucketCounts[bucket] += 1
+            comments.append(comment["response_time_hours"])
+            labels.append(bucket)
+
+    return comments, labels
+
+if __name__ == "__main__":
+    print "Loading Training Data"
+    trainComments, trainLabels = loadComments(
+        sys.argv[1] + "/Reddit2ndTrain",
+        1000000)
+
+    print "Loading Dev Data"
+    devComments, devLabels = loadComments(
+        sys.argv[1] + "/Reddit2ndDev",
+        100000)
+
+    softmaxRegression(trainComments, trainLabels, devComments, devLabels)
