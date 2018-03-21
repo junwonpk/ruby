55a326a5ab7d24acefa00b7b64416cc97e90eb8f
Junwon Park <junwonpk@stanford.edu>
2018-03-21 10:12:07 +0800

    a

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git filter-branch -f --env-filter '
 echo $GIT_COMMITTER_EMAIL
-OLD_EMAIL="john@Ruby.nfwnhrqpw3tepcqoskly4pbn4e.jx.internal.cloudapp.net"
+OLD_EMAIL="junwonpk@OrbisAzure.3agi02t3tv4e3fw3mmfk1lloag.xx.internal.cloudapp.net"
 CORRECT_NAME="Junwon Park"
 CORRECT_EMAIL="junwonpk@stanford.edu"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
