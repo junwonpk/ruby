6ded2159d7a2ea346ca0b4ffed39670e86f84e65
Junwon Park <junwonpk@stanford.edu>
2018-03-21 10:00:43 +0800

    correcting author info

---
+++ git.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+echo $GIT_AUTHOR_EMAIL
+OLD_EMAIL="john@Ruby2.nfwnhrqpw3tepcqoskly4pbn4e.jx.internal.cloudapp.net"
+CORRECT_NAME="Junwon Park"
+CORRECT_EMAIL="junwonpk@stanford.edu"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
