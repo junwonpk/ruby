e6f6d8fcf4d06d925753f3c2a9b37e4fe8502093
Junwon Park <junwonpk@stanford.edu>
2018-03-21 10:20:42 +0800

    a

--- git.sh
+++ git.sh
@@ -1,14 +1,14 @@
 git filter-branch -f --env-filter '
 echo $GIT_COMMITTER_EMAIL
-OLD_EMAIL="junwonpk@hprb-rtr.stanford.edu"
+OLD_EMAIL="yhjw88@gmail.com"
 CORRECT_NAME="Junwon Park"
 CORRECT_EMAIL="junwonpk@stanford.edu"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" != "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" != "$OLD_EMAIL" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
